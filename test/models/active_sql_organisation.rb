02b2be5d9d50bb15174b7e4ce6490dc5a82aed10
Christian Eichhorn <ceichhor@laptop.(none)>
2013-02-25 01:35:20 +0100

    * test models bug fixed

--- test/models/active_sql_organisation.rb
+++ test/models/active_sql_organisation.rb
@@ -3,16 +3,16 @@ class ActiveSqlOrganisation < ActiveRecord::Base
   has_one :head, :class_name => "ActiveSqlPerson", :foreign_key => 'head_id'
 
   has_many :active_sql_notebooks_from_employees, :through => :employees, 
-    :source => :active_sql_notebooks, :class_name => 'ActiveSqlNotebook'
+    :source => :active_sql_notebook, :class_name => 'ActiveSqlNotebook'
   
   has_many :active_sql_sub_notebooks_from_employees, :through => :employees, 
-    :source => :active_sql_sub_notebooks
+    :source => :active_sql_sub_notebook
 
   has_many :active_sql_notebooks_from_head, :through => :head, 
-    :source => :active_sql_notebooks, :class_name => 'ActiveSqlNotebook'
+    :source => :active_sql_notebook, :class_name => 'ActiveSqlNotebook'
   
   has_many :active_sql_sub_notebooks_from_head, :through => :head, 
-    :source => :active_sql_sub_notebooks
+    :source => :active_sql_sub_notebook
 
   has_many :paid_active_sql_notebooks, :as => :paying_partner,
     :class_name => "ActiveSqlNotebook"
