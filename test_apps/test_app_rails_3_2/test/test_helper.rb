e85df0ff32dbfa3c27b773ccfc5a7bf90a062a3b
Christian Eichhorn <c.eichhorn@webmasters.de>
2013-08-17 18:03:25 +0200

    * test_app_rails_3_2   * configured for tests with active_sql

--- test_apps/test_app_rails_3_2/test/test_helper.rb
+++ test_apps/test_app_rails_3_2/test/test_helper.rb
@@ -2,12 +2,11 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
 
-  # Add more helper methods to be used by all tests here...
+Dir[File.expand_path(File.dirname(__FILE__) + "/../../../test/**/*_test.rb")].sort.each do |model|
+  require model
 end
+
+#class ActiveSupport::TestCase
+#end
