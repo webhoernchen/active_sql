bb307b571440aca620f89f4ce528d40901eb4536
Christian Eichhorn <c.eichhorn@webmasters.de>
2018-07-02 14:59:27 +0200

    * ActiveSql::Condition    * fix for rails 5.2 added

--- lib/active_sql/condition.rb
+++ lib/active_sql/condition.rb
@@ -524,7 +524,7 @@ module ActiveSql
     def extract_conditions_from_arel_relation(relation)
       arel = relation.arel
       if where_sql = arel.where_sql
-        values = relation.where_values_hash.values
+        values = relation.where_values_hash.values.flatten
         where_sql = relation.to_sql if values.empty? && where_sql.include?('= ?')
         where_sql = where_sql.split('WHERE')[1..-1].join('WHERE').strip
 
